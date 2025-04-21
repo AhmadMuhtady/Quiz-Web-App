3c5b5bb0db703874a711ff1d2c9cca83ab78630f
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-21 18:39:38 +0300

    displayed welcome message for logged-in users in home.js

--- home.js
+++ home.js
@@ -2,3 +2,6 @@ const currentUser = JSON.parse(localStorage.getItem('currentUser'));
 if (!currentUser || currentUser.isAdmin) {
 	window.location.href = 'index.html';
 }
+document.querySelector(
+	'h1'
+).textContent = `🧑🏻‍🎓 Welcome, ${currentUser.firstName}!`;
