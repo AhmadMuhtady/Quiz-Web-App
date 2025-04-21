add84dd0aa2c07ad418a736c963ee1e26e442276
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-21 16:53:27 +0300

    added login and registration form elements in script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const loginTab = document.getElementById('show-register');
+const registerTab = document.getElementById('show-login');
+const loginForm = document.querySelector('.login-container');
+const registerForm = document.querySelector('.register-container');
