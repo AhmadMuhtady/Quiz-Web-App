cf618060d0bb46d4c07117fcbadcf7d9966abaf0
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-04-21 16:56:06 +0300

    added event listener for login tab to toggle visibility of forms

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ const loginTab = document.getElementById('show-register');
 const registerTab = document.getElementById('show-login');
 const loginForm = document.querySelector('.login-container');
 const registerForm = document.querySelector('.register-container');
+
+loginTab.addEventListener('click', () => {
+	loginForm.classList.add('hidden');
+	registerForm.classList.remove('hidden');
+});
